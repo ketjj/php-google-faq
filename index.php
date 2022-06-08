c9d3228574a9d9d355130121454d1466ec97355e
ketjj <jorjolianiket@gmail.com>
2022-06-08 16:12:11 +0200

    init

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+include 'faq.php';
+// var_dump($faqs);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="style.css" rel="stylesheet" type="text/css"></link>
+  <title>php-google-faq</title>
+</head>
+<body>
+  <h1 class="red">
+   123
+  </h1>
+  
+</body>
+</html>
