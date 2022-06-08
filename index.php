4a55e1beeb61d996e824975861d273b4dbc0a7f1
ketjj <jorjolianiket@gmail.com>
2022-06-08 17:45:59 +0200

    adding header

--- index.php
+++ index.php
@@ -10,13 +10,52 @@ include 'faq.php';
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+
+  <!-- BOOTSTRAP -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  
+  <!-- STYLE CSS -->
   <link href="style.css" rel="stylesheet" type="text/css"></link>
   <title>php-google-faq</title>
 </head>
 <body>
-  <h1 class="red">
-   123
-  </h1>
-  
+  <header>
+
+    <div class="upper-header d-flex align-items-center justify-content-between">
+      <div class="left d-flex align-items-center">
+        <a href="#">
+          <img src="img/Google_2015_logo.svg.png" alt="">
+        </a>
+        <a href="#" class="privacy">
+          <span>Privacy e termini</span>
+        </a>
+      </div>
+
+      <div class="right d-flex align-items-center">
+          <a href="#">
+            <i class="fa-solid fa-ellipsis-vertical"></i>
+            <i class="fa-solid fa-ellipsis-vertical"></i>
+            <i class="fa-solid fa-ellipsis-vertical"></i>
+          </a>
+          <button class="ml-4">K</button>
+      </div>
+    </div>
+
+    <div class="lower-header">
+      <nav>
+        <ul class="d-flex justify-content-between">
+          <li><a href="#">Introduzione</a></li>
+          <li><a href="#">Norme sulla privacy</a></li>
+          <li><a href="#">Termini di servizio</a></li>
+          <li><a href="#">Tecnologie</a></li>
+          <li><a href="#" id="active">Domande frequenti</a></li>
+        </ul>
+      </nav>
+    </div>
+
+  </header>
+  <main>34</main>
 </body>
 </html>
